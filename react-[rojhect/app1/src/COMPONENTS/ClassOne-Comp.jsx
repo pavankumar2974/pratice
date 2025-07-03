89880c6e30ad69b178861247daad326492c6af24
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-03 22:15:20 +0530

    Reactjs

---
+++ react-[rojhect/app1/src/COMPONENTS/ClassOne-Comp.jsx
@@ -0,0 +1,15 @@
+// import { Component } from "react";
+
+
+
+
+// class ClassOneComp extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h2>Welcome to Class1 component</h2>
+//             </div>
+//         )
+//     }
+// }
+// export default ClassOneComp;
---
+++ react-[rojhect/app1/src/COMPONENTS/ClassTwoComp.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+
+
+
+class ClassTwoComp extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Adhivik</h2>
+                <h2>Koushik</h2>
+                <h2>Pavan</h2>
+                <h2>Aruna</h2>
+                <h2>Sathish</h2>
+                <h2>Sakunthala</h2>
+                <h2>Mohana</h2>
+                <h2>Shallu</h2>
+                <h2>Ganvitha</h2>
+                <h2>BommaCar</h2>
+                <h2>JCB,Tractor</h2>
+                <h2></h2>
+            </div>
+        )
+    }
+}
+export default ClassTwoComp;
