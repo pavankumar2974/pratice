b76fd093264d724c3580cf4153c4dd00e2ae454f
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-07 21:32:10 +0530

    react

--- react-[rojhect/app1/src/App.jsx
+++ react-[rojhect/app1/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ClassTwoComp from './COMPONENTS/ClassTwoComp'
+import ClassOneComp from './COMPONENTS/ClassOneComp'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-     <ClassTwoComp/>
+     <ClassOneComp/>
     </div>
   )
 }
