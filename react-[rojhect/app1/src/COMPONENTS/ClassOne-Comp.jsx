bbc6f0d185aa8d5a977c270be79f15562a7617c8
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-09 10:37:36 +0530

    React

--- react-[rojhect/app1/src/COMPONENTS/ClassOne-Comp.jsx
+++ react-[rojhect/app1/src/COMPONENTS/ClassOne-Comp.jsx
@@ -1,15 +1,15 @@
-// import { Component } from "react";
+import { Component } from "react";
 
 
 
 
-// class ClassOneComp extends Component{
-//     render(){
-//         return(
-//             <div>
-//                 <h2>Welcome to Class1 component</h2>
-//             </div>
-//         )
-//     }
-// }
-// export default ClassOneComp;
+class ClassOneComp extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Welcome to Class1 component</h2>
+            </div>
+        )
+    }
+}
+export default ClassOneComp;
