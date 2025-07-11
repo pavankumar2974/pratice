fa5c87f326e0abffbcb768e2d8f53d84b112aee2
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-11 14:21:54 +0530

    ReactClassComponents

---
+++ game2/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import GameComp from './assets/components/gameComp.jsx'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <GameComp />
+    </>
+  )
+}
+
+export default App;
---
+++ game2/src/assets/components/GameComp.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+
+
+
+class GameComp extends Component{
+    render(){
+        return(
+            <div>
+          <h1>Game  character</h1>      
+          <ul>
+            <li>Character 1:- Deva from Salaar</li>
+            <li>Character 2:-Mahendra Bhaubali from Bhaubali</li>
+            <li>Character 3:- Bhairava from Kaliki</li>
+            <li>Character 4:- Praba from darling</li>
+          </ul>
+            </div>
+        )
+    }
+}
+export default GameComp;
