90e1658e60a224f6fcde6b51990952612ea97246
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-11 11:57:59 +0530

    React Components

--- game1/src/App.jsx
+++ game1/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ClassOneComp  from './assets/components/ClassOneComp.jsx'
+import ClassOneComp  from './assets/components/ClassOneCom.jsx'
 function game1() {
   const [count, setCount] = useState(0)
 
