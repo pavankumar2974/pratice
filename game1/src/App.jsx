efd0a5d8b4e4cddf9233ea33cd87d66687d4015a
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-11 11:49:58 +0530

    React

---
+++ game1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ClassOneComp  from './assets/components/ClassOneComp.jsx'
+function game1() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <ClassOneComp/>
+    </>
+  )
+}
+
+export default game1;
