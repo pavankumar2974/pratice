c6444dc7655ff0c2b2fee5d3462c0543cac71460
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-12 22:00:45 +0530

    React

--- .vscode/React-js/game2/src/App.jsx
+++ .vscode/React-js/game2/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import GameComp from 'assets./components/gameComp.js'
+import GameComp from 'src./components/gameComp.js'
 function App() {
   const [count, setCount] = useState(0)
 
