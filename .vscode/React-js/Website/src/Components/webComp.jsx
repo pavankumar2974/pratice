36a92c2940a3bd7fd18f98d1b62b1c9114e112ec
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-15 20:20:03 +0530

    Reactjs

---
+++ .vscode/React-js/Website/src/Components/webComp.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class WebComp extends Component {
+  render() {
+    return<div> <h1>Movie Manager</h1>
+        <br></br>
+
+     </div>
+  }
+}
+
+export default WebComp;
