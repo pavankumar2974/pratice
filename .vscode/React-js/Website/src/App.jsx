a9767643b12d29cabc04168f164f9f28052e30f0
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-16 20:51:56 +0530

    Reactjs

--- .vscode/React-js/Website/src/App.jsx
+++ .vscode/React-js/Website/src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WebComp from 'src/Components/webComp.jsx'
-
+import webComp from 'src./components/webComp.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <WebComp />
+      <webComp />
     </>
   )
 }
 
-export default App
+export default App;
