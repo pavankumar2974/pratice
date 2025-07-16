0c47a81543857551d1f58d869dfe01eb46e0efac
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-16 20:50:53 +0530

    Reactjs

---
+++ .vscode/React-js/app1/src/COMPONENTS/ClassOneComp.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+class ClassOneComp extends Component {
+    constructor(){
+        super();
+        this.state = {
+            message: "Welcome to ReactJs State",
+        };
+    }
+    render(){
+        return (
+            <div>
+                <h2>{this.state.message}</h2>
+            </div>
+        );
+    }
+}
+
+export default ClassOneComp;
