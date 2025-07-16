27fb14dde43983f9d166d42eb99f8273b169824c
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-16 20:50:11 +0530

    Reactjs

---
+++ .vscode/React-js/app1/src/COMPONENTS/ClassBaseComp.jsx
@@ -0,0 +1,12 @@
+import React,{ Component} from "react";
+class classBaseComp extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Welcome to class-based component</h2>
+            </div>
+        )
+    }
+}
+
+export default classBaseComp;
