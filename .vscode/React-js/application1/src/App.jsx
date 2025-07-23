45eeff72a305ade51d2893ed6ac7e352bc913084
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-23 21:17:41 +0530

    React-js

--- .vscode/React-js/application1/src/App.jsx
+++ .vscode/React-js/application1/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ComponentOneClass from './COMPONENTS/ComponentOneClass'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ClassBaseComp from "./COMPONENTS/ClassBaseComp";
+import PavanClassComp from "./COMPONENTS/PavanClassComp";
+import {
+  KumarClassComp,
+  LeelaClassComp,
+  VMRClassComp,
+} from "./COMPONENTS/LeelaClassComp";
+import Employee from "./component/Employee.jsx";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <ComponentOneClass />
+      <div>
+        {/* <ClassBaseComp />
+        <PavanClassComp /> */}
+        {/* <LeelaClassComp />
+        <KumarClassComp />
+        <VMRClassComp /> */}
+        <Employee />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
