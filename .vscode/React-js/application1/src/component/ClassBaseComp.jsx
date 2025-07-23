b96f8776581ac5cfc09037cc468a992a1c197945
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-23 21:17:54 +0530

    React-js

---
+++ .vscode/React-js/application1/src/component/ClassBaseComp.jsx
@@ -0,0 +1,17 @@
+// This Is JAvascript Narmal Class
+// class classBaseComp{}
+
+import React, { Component } from "react";
+
+// How to Convert Narmal JS Class to Reactjs Component
+class ClassBaseComp extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to ReactJs ClassBaseComp</h2>
+      </div>
+    );
+  }
+}
+
+export default ClassBaseComp;
