9e22e1dd500cce49567492d648071761cd060993
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-25 09:50:35 +0530

    React-js

---
+++ .vscode/React-js/application/src/components/Comp.jsx
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+class Comp extends Component{
+    constructor(){
+        super();
+        this.state={
+            Car:[
+                "INNOVA",
+                "ERTIGA",
+                "SCARPIO",
+                "NANO",
+                "BMW",
+                "AUDI",
+
+],
+        };
+    }
+
+    render(){
+        return(
+            <div>
+                <ul>
+                 {Object.values(this.state.Car).map((val,i)=>{
+                    return<li key={i}>{val}</li>;
+                 })}   
+                </ul>
+            </div>
+        );
+    }
+}
+export default Comp;
