4c3eef411627617113e99947c2499374f5a60629
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-25 09:38:15 +0530

    React-js

--- .vscode/React-js/application/src/components/appComp.jsx
+++ .vscode/React-js/application/src/components/appComp.jsx
@@ -1,4 +1,32 @@
 import { Component } from "react";
-class Application extends Component{
-    
+
+class AppComp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            Bikes: [
+                "TVS",
+                "Bajaj",
+                "Suzuki",
+                "Hero",
+                "Honda",
+                "KTM",
+                "RajDooth",
+                "Yezdi",
+            ],
+        };
+    }
+    render() {
+        return (
+            <div>
+                <ul>
+                    {Object.values(this.state.Bikes).map((val,i)=>{
+                        return<li key={i}>{val}</li>;
+                    })
+                }
+                </ul>
+            </div>
+        );
+    }
 }
+export default AppComp;
