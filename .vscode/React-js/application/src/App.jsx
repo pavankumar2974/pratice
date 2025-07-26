743941f626bc6b00b463ff7a36999441250e1e12
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-26 12:21:23 +0530

    React-js

--- .vscode/React-js/application/src/App.jsx
+++ .vscode/React-js/application/src/App.jsx
@@ -5,15 +5,27 @@ import './App.css'
 import AppComp from './components/appComp'
 import Comp from './components/Comp'
 import Compo from './components/Compo'
+import ClassCar from './components/ClassCar'
+import ClassBike from './components/ClassBike'
+import SampleClass, { Cars, Example } from './components/SampleClass'
+import Bike from './components/SampleClass'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <><div>
-<AppComp/>
+{/* <AppComp/>
 <Comp/>
 <Compo/>
+<ClassCar/> */}
+<ClassBike/>
+<SampleClass/>
+<Bike/>
+<Cars/>
+<Example/>
     </div>  
     </>
   )
