a712cc6d344afa687bddb6f97bdc34e5bf840e75
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-26 12:58:15 +0530

    React-js

--- .vscode/React-js/application/src/components/SampleClass.jsx
+++ .vscode/React-js/application/src/components/SampleClass.jsx
@@ -1,32 +1,32 @@
 import { Component } from "react";
 
 
- export class SampleClass extends Component{
-    render(){
-        return<div>
+export class SampleClass extends Component {
+    render() {
+        return <div>
             <h3>Welcome to Sample Class Component</h3>
         </div>
     }
 };
 
-export default class Bike extends Component{
-    render(){
+export default class Bike extends Component {
+    render() {
         return <div>
             <h2>Welcome to bike Component</h2>
         </div>
     }
 };
 
-export class Cars extends Component{
-    render(){
+export class Cars extends Component {
+    render() {
         return <div>
             <h1>Welcome to Car Component</h1>
         </div>
     }
 };
 
-export class Example extends Component{
-    render(){
+export class Example extends Component {
+    render() {
         return <div>
             <h1>Welcome to Example Component</h1>
         </div>
