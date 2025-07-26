b8fbee61f0bcec82603e8c35957abc1feaf056e3
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-26 12:22:23 +0530

    React-js

---
+++ .vscode/React-js/application/src/components/ClassCar.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+
+class ClassCar extends Component{
+    render(){
+        return(<div><h1>Welcome to ClassCar jsx</h1></div>);
+    }
+
+};
+export default ClassCar;
