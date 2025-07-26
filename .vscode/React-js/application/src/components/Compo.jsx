e2f5c5512acc94fa6f2eb3de918aa68479db3337
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-26 12:58:00 +0530

    React-js

--- .vscode/React-js/application/src/components/Compo.jsx
+++ .vscode/React-js/application/src/components/Compo.jsx
@@ -1,46 +1,46 @@
 import { Component } from "react";
 
-class Compo extends Component{
-    constructor(){
+class Compo extends Component {
+    constructor() {
         super();
-        this.state ={
-            lang:[
+        this.state = {
+            lang: [
                 "c",
                 "c++",
                 "Java",
                 "Python",
                 "R",
             ],
-            message:"Welcome to Compo.jsx",
+            message: "Welcome to Compo.jsx",
 
-            languages:{
-                Pro:"c,c++,python",
-                dataBase:"SQL,MangoDB",
-                Testing:"Selunium",
-                cloud:"Devops,Azure",
+            languages: {
+                Pro: "c,c++,python",
+                dataBase: "SQL,MangoDB",
+                Testing: "Selunium",
+                cloud: "Devops,Azure",
             }
 
         };
     }
-render(){
-    return(
-        <div>
-            <ul>
-                {Object.values(this.state.lang).map((val,i)=>{
-                    return<li key={i}>{val}</li>
-                })}
-                
-            </ul>
-            <h2>{this.state.message}</h2>
-            <ul>
-                <li>{this.state.languages.Pro}</li>
-                <li>{this.state.languages.Testing}</li>
-                <li>{this.state.languages.cloud}</li>
-                <li>{this.state.languages.dataBase}</li>
-            </ul>
-        </div>
-    );
-}
-    
+    render() {
+        return (
+            <div>
+                <ul>
+                    {Object.values(this.state.lang).map((val, i) => {
+                        return <li key={i}>{val}</li>
+                    })}
+
+                </ul>
+                <h2>{this.state.message}</h2>
+                <ul>
+                    <li>{this.state.languages.Pro}</li>
+                    <li>{this.state.languages.Testing}</li>
+                    <li>{this.state.languages.cloud}</li>
+                    <li>{this.state.languages.dataBase}</li>
+                </ul>
+            </div>
+        );
+    }
+
 }
 export default Compo;
