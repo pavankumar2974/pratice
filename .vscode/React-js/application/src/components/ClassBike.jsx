7293ff62fd6f427e5cba1f582b2060ad7e73b43a
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-26 12:21:39 +0530

    React-js

---
+++ .vscode/React-js/application/src/components/ClassBike.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+
+
+
+class ClassBike extends Component{
+    render(){
+        return <div>
+            <h1>Welcome to ClassBkeComponent</h1>
+        </div>
+    }
+}
+
+export default ClassBike;
