50ed65d4fd256f28154675c78ed0584442b2aa3b
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-26 12:57:04 +0530

    React-js

--- .vscode/React-js/application/src/components/ClassBike.jsx
+++ .vscode/React-js/application/src/components/ClassBike.jsx
@@ -3,8 +3,8 @@ import { Component } from "react";
 
 
 
-class ClassBike extends Component{
-    render(){
+class ClassBike extends Component {
+    render() {
         return <div>
             <h1>Welcome to ClassBkeComponent</h1>
         </div>
