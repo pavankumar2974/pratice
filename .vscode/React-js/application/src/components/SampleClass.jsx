cb48a6881beb8a305c2e4c22d69cc59c1658bcc1
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-26 12:22:38 +0530

    React-js

---
+++ .vscode/React-js/application/src/components/SampleClass.jsx
@@ -0,0 +1,40 @@
+import { Component } from "react";
+
+
+ export class SampleClass extends Component{
+    render(){
+        return<div>
+            <h3>Welcome to Sample Class Component</h3>
+        </div>
+    }
+};
+
+export default class Bike extends Component{
+    render(){
+        return <div>
+            <h2>Welcome to bike Component</h2>
+        </div>
+    }
+};
+
+export class Cars extends Component{
+    render(){
+        return <div>
+            <h1>Welcome to Car Component</h1>
+        </div>
+    }
+};
+
+export class Example extends Component{
+    render(){
+        return <div>
+            <h1>Welcome to Example Component</h1>
+        </div>
+    }
+}
+
+
+
+// export default SampleClass;
+
+
