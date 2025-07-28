bf622abeb1af876560364a64330df4206b033e20
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-28 12:25:42 +0530

    React-js

---
+++ .vscode/React-js/app3/src/component/AppClassComp.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+class AppClassComp extends Component{
+    render(){
+return<div>
+    <div id="head">
+        <h1>Movie Collection</h1>
+    </div>
+</div>
+    }
+}
+export default AppClassComp;
