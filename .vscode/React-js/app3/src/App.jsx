5002e93a32a45d5aab08f222006b26c692477302
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-01 21:22:51 +0530

    React-js

--- .vscode/React-js/app3/src/App.jsx
+++ .vscode/React-js/app3/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AppClassComp from './component/appClassComp'
+// import AppClassComp from './component/appClassComp'
 import ParentComp from './component/parentComp'
 
 
