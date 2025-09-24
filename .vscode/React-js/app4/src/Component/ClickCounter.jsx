34b9b3803e940c4107974c8fe727c9ac469d2909
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-24 20:47:21 +0530

    HOCComponent

---
+++ .vscode/React-js/app4/src/Component/ClickCounter.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import HocComponent from "./HocComponent";
+
+class ClickCounter extends Component {
+    render() {
+        console.log(this.props);
+        console.log("From ClickCounter");
+        
+        
+        const { count, incrementCount, decrementCount, resetCount } = this.props;
+
+        return (
+            <div>
+                <button onClick={incrementCount}>Increment</button>
+                <button onClick={decrementCount}>Decrement</button>
+                <button onClick={resetCount}>Reset</button>
+                <h2>Count: {count}</h2>
+            </div>
+        );
+    }
+}
+
+export default HocComponent(ClickCounter);
