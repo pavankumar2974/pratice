7c1503d4df9807ae76c6123391ca8b6475b69849
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-24 20:49:07 +0530

    HOCComponent

--- .vscode/React-js/app4/src/Component/HoverCounter.jsx
+++ .vscode/React-js/app4/src/Component/HoverCounter.jsx
@@ -3,19 +3,19 @@ import HocComponent from "./HocComponent";
 
 
 
-class HoverCounter extends Component{
-    render(){
+class HoverCounter extends Component {
+    render() {
         console.log(this.props);
         console.log("HoverCounter");
-        const {count,incrementCount,decrementCount,resetCount}=this.props;
-    return(
-<div>
-    <button onMouseOver={incrementCount}>Increment</button>
-    <button onMouseOver={decrementCount}>Decrement</button>
-    <button onMouseOver={resetCount}>Reset</button>
-    <h2>Count:{count}</h2>
-</div>
-    )    
-}
+        const { count, incrementCount, decrementCount, resetCount } = this.props;
+        return (
+            <div>
+                <button onMouseOver={incrementCount}>Increment</button>
+                <button onMouseOver={decrementCount}>Decrement</button>
+                <button onMouseOver={resetCount}>Reset</button>
+                <h2>Count:{count}</h2>
+            </div>
+        )
+    }
 }
 export default HocComponent(HoverCounter);
