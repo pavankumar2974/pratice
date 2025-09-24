0bef79b39e0a94800d2cd2f7e261981940571572
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-24 20:47:45 +0530

    HOCComponent

---
+++ .vscode/React-js/app4/src/Component/HocComponent.jsx
@@ -0,0 +1,34 @@
+import { Component } from "react";
+
+
+function HocComponent(Middlecomp){
+    return class extends Component{
+        constructor(props){
+            super(props);
+            this.state={
+                count:0,
+            };
+        }
+        incrementCount=()=>{
+            this.setState({count:this.state.count+1});
+        };
+        decrementCount=()=>{
+            this.setState({count:this.state.count-1});
+        };
+        resetCount=()=>{
+            this.setState({count:0});
+        };
+        render(){
+            return(
+                <div>
+                    <Middlecomp  
+                    incrementCount={this.incrementCount}
+                    decrementCount={this.decrementCount}
+                    resetCount={this.resetCount}
+                    {...this.state}/>
+                </div>
+            )
+        }
+    }
+}
+export default HocComponent;
